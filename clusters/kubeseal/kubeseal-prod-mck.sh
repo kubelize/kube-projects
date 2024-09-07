f99061b3477d9c2a8a209a7da026475fe5431bda
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-09-07 14:29:08 +0200

    chore: added secret for wikijs on prod-mck

---
+++ clusters/kubeseal/kubeseal-prod-mck.sh
@@ -0,0 +1,4 @@
+kubeseal --format=yaml \
+  --cert=pub-prod-mck-sealed-secrets.pem \
+  --secret-file /home/dan/Git/kubelize/kube-projects/projects/incubator/webservices/wikijs/secret.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-projects/projects/incubator/webservices/wikijs/secret.yaml
